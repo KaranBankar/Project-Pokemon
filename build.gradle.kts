0effcc8871c141f020c4e370a9ffd6e6fc0a321f
KaranBankar <karanbankar54@gmail.com>
2024-07-14 21:51:16 +0530

    xyz

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
-<<<<<<< HEAD
+
    alias(libs.plugins.jetbrains.kotlin.android) apply false
    // id("org.jetbrains.kotlin.android") version "2.0.0" apply false
-=======
+
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     //id("org.jetbrains.kotlin.android") version "2.0.0" apply false
->>>>>>> a0d4aea3561fb45bd74b798c73592ae3a78b830a
+
 }
