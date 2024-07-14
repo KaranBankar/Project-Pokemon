620c0155c99aee64707c772de71d4d76099b8ee6
ChaitanyKakde <chaitnykakde517@gmail.com>
2024-07-14 21:21:50 +0530

    l

---
+++ app/src/main/java/com/example/pokemonusingkotli/Adapter/newadapterkaran.kt
@@ -0,0 +1,4 @@
+package com.example.pokemonusingkotli.Adapter
+
+class newadapterkaran {
+}
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,5 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
+    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
 }
